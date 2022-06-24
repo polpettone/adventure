7e5380048617c43f04979872dcc12bafd7887181
Stefan Heiden <stefanheiden@gmail.com>
2022-06-24 18:03:24 +0200

    moving

--- main.go
+++ main.go
@@ -7,16 +7,25 @@ import (
 	"strings"
 )
 
+var gameMap Map
+var bot Bot
+
 func main() {
+	start()
+}
 
-	var engine TextEngine = SimpleEngine{}
-	m := InitMap()
-	m.Place(NewBot(GOPHER), 0, 0)
+func start() {
 
+	bot := NewBot(GOPHER, 0, 0)
+	gameMap := NewMap(10, 10)
+	gameMap.Place(bot)
+	fmt.Println(gameMap.Print())
+
+	var engine Engine = SimpleEngine{
+		GameMap: gameMap,
+		Bot:     bot,
+	}
 	reader := bufio.NewReader(os.Stdin)
-
-	fmt.Println(m.Print())
-
 	for {
 		fmt.Print("")
 		text, _ := reader.ReadString('\n')
@@ -33,27 +42,59 @@ func main() {
 	}
 }
 
-type TextEngine interface {
+type Engine interface {
 	Machine(text string) string
 }
 
-type SimpleEngine struct{}
-
-func (se SimpleEngine) Machine(text string) string {
-	return "ich kann gar nix"
+type SimpleEngine struct {
+	GameMap Map
+	Bot     *Bot
 }
 
-func InitMap() Map {
-	fields := make([][]MapElement, 10)
+func (se SimpleEngine) Machine(text string) string {
 
-	for n := range fields {
-		fields[n] = make([]MapElement, 10)
-	}
+	switch text {
 
-	for x := 0; x < 10; x++ {
-		for y := 0; y < 10; y++ {
-			fields[x][y] = NewField(FIELD)
+	case "k":
+		if se.Bot.Y == se.GameMap.MaxY-1 {
+			return "wall, cant move"
 		}
+		se.GameMap.Place(NewField(FIELD, se.Bot.X, se.Bot.Y))
+		se.Bot.Y += 1
+		se.GameMap.Place(se.Bot)
+		fmt.Println(se.GameMap.Print())
+		return "moved up"
+
+	case "l":
+		if se.Bot.X == se.GameMap.MaxX-1 {
+			return "wall, cant move"
+		}
+		se.GameMap.Place(NewField(FIELD, se.Bot.X, se.Bot.Y))
+		se.Bot.X += 1
+		se.GameMap.Place(se.Bot)
+		fmt.Println(se.GameMap.Print())
+		return "moved right"
+
+	case "h":
+		if se.Bot.X == 0 {
+			return "wall, cant move"
+		}
+		se.GameMap.Place(NewField(FIELD, se.Bot.X, se.Bot.Y))
+		se.Bot.X -= 1
+		se.GameMap.Place(se.Bot)
+		fmt.Println(se.GameMap.Print())
+		return "moved up"
+
+	case "j":
+		if se.Bot.Y == 0 {
+			return "wall, cant move"
+		}
+		se.GameMap.Place(NewField(FIELD, se.Bot.X, se.Bot.Y))
+		se.Bot.Y -= 1
+		se.GameMap.Place(se.Bot)
+		fmt.Println(se.GameMap.Print())
+		return "moved up"
 	}
-	return Map{Fields: fields}
+
+	return "nothing happend"
 }
--- models.go
+++ models.go
@@ -6,10 +6,31 @@ import (
 
 type MapElement interface {
 	GetSymbol() rune
+	GetX() int
+	GetY() int
 }
 
 type Map struct {
 	Fields [][]MapElement
+	MaxX   int
+	MaxY   int
+}
+
+func NewMap(maxX, maxY int) Map {
+
+	fields := make([][]MapElement, maxX)
+	for n := range fields {
+		fields[n] = make([]MapElement, maxY)
+	}
+	m := Map{Fields: fields, MaxX: maxX, MaxY: maxY}
+	for x := 0; x < maxX; x++ {
+		for y := 0; y < maxY; y++ {
+			field := NewField(FIELD, x, y)
+			m.Place(field)
+		}
+	}
+	return m
+
 }
 
 func (m Map) Print() string {
@@ -23,19 +44,23 @@ func (m Map) Print() string {
 	return s
 }
 
-func (m Map) Place(elem MapElement, x, y int) MapElement {
-	prevElem := m.Fields[x][y]
-	m.Fields[x][y] = elem
+func (m Map) Place(elem MapElement) MapElement {
+	prevElem := m.Fields[elem.GetX()][elem.GetY()]
+	m.Fields[elem.GetX()][elem.GetY()] = elem
 	return prevElem
 }
 
 type Field struct {
 	Symbol rune
+	X      int
+	Y      int
 }
 
-func NewField(symbol rune) Field {
+func NewField(symbol rune, x, y int) Field {
 	return Field{
 		Symbol: symbol,
+		X:      x,
+		Y:      y,
 	}
 }
 
@@ -43,16 +68,36 @@ func (f Field) GetSymbol() rune {
 	return f.Symbol
 }
 
-type Bot struct {
-	Symbol rune
+func (f Field) GetX() int {
+	return f.X
 }
 
-func NewBot(symbol rune) Field {
-	return Field{
+func (f Field) GetY() int {
+	return f.Y
+}
+
+type Bot struct {
+	Symbol rune
+	X      int
+	Y      int
+}
+
+func NewBot(symbol rune, x, y int) *Bot {
+	return &Bot{
 		Symbol: symbol,
+		X:      x,
+		Y:      y,
 	}
 }
 
 func (b Bot) GetSymbol() rune {
 	return b.Symbol
+}
+
+func (b Bot) GetX() int {
+	return b.X
+}
+
+func (b Bot) GetY() int {
+	return b.Y
 }
