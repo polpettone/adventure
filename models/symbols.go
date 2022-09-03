f618ad0c33742dbf69e4a55bf326d3374b16b561
Stefan Heiden <stefanheiden@gmail.com>
2022-09-03 19:07:08 +0200

    refactoring

--- models/symbols.go
+++ models/symbols.go
@@ -7,35 +7,39 @@ const WATER = "~"
 const BOX = ""
 const GITHUB = ""
 
+var PLAYER = WHALE
+var PLAYER2 = ELEPHANT
+
 const FIELD_SYMBOL = " "
+
+const FIELD_WIDTH = 2
 
 var FIELD string = strings.Repeat(FIELD_SYMBOL, FIELD_WIDTH)
 
 const ELEPHANT_SYMBOL = "🐘"
 
-var ELEPHANT string = ELEPHANT_SYMBOL + strings.Repeat(FIELD_SYMBOL, FIELD_WIDTH-len(ELEPHANT_SYMBOL)+2)
+var ELEPHANT string = transformSymbol(ELEPHANT_SYMBOL, FIELD, FIELD_WIDTH, 2)
 
 const WHALE_SYMBOL = "🐋"
 
-var WHALE string = WHALE_SYMBOL + strings.Repeat(FIELD_SYMBOL, FIELD_WIDTH-len(WHALE_SYMBOL)+2)
+var WHALE string = transformSymbol(WHALE_SYMBOL, FIELD, FIELD_WIDTH, 2)
 
 const PENGUIN_SYMBOL = "🐧"
 
-var PENGUIN string = PENGUIN_SYMBOL + strings.Repeat(FIELD_SYMBOL, FIELD_WIDTH-len(PENGUIN_SYMBOL)+2)
+var PENGUIN string = transformSymbol(PENGUIN_SYMBOL, FIELD, FIELD_WIDTH, 2)
 
 const HEART = "❤️'"
 
 const BALLON_SYMBOL = "🎈"
 
-var BALLON string = BALLON_SYMBOL + strings.Repeat(FIELD_SYMBOL, FIELD_WIDTH-len(BALLON_SYMBOL)+2)
-
-var PLAYER = WHALE
-var PLAYER2 = ELEPHANT
-
-const FIELD_WIDTH = 2
+var BALLON string = transformSymbol(BALLON_SYMBOL, FIELD, FIELD_WIDTH, 2)
 
 const KAPUTT_SYMBOL = "💢"
 
-var KAPUTT string = KAPUTT_SYMBOL + strings.Repeat(KAPUTT_SYMBOL, FIELD_WIDTH-len(KAPUTT_SYMBOL)+2)
+var KAPUTT string = transformSymbol(KAPUTT_SYMBOL, FIELD, FIELD_WIDTH, 2)
 
 const SYMBOLE = "💘💝💖💗💓💞💕💟❣️💔❤️🧡💛💚💙💜🤎🖤🤍💯💢👁️‍🗨️💬🗨️🗯️💭💤💮♨️💈🛑🕛🕧🕐🕜🕑🕝🕒🕞🕓🕟🕔🕠🕕🕡🕖🕢🕗🕘🕤🕙🕥🕚🕦🌀🃏🀄🎴🔇🔈🔉🔊📢📣📯🔔🔕🎵🎶🚮🚰♿🚹🚺🚻🚼🚾⚠️🚸⛔🚫🚳🚭🚯🚱🔞☢️☣️✖️➕➖➗❓❔❗❕🔴🟠🟡🟢🔵🟣🟤⚫⚪🔘🟥🟧🟨🟩🟦🟪🟫⬛⬜🔳🔲▪️▫️◾️◽️◼️◻️ "
+
+func transformSymbol(symbol string, fieldSymbol string, fieldWith int, scaleValue int) string {
+	return symbol + strings.Repeat(fieldSymbol, fieldWith-len(symbol)+scaleValue)
+}
